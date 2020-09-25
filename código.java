f918c4f8ed51c84a80b50ca6d32d6c4b9f57e9b6
Eduaddad <duduaddad@gmail.com>
2020-09-24 22:37:27 -0300

    Update código.java

--- código.java
+++ código.java
@@ -3,27 +3,29 @@ package banco;
 import java.util.Scanner;
 
 public class banco {
-public static void main(String[] args) {
-int cpf, senha, opcao;
-System.out.println("Bem-vindo ao caixa eletrônico");
+        public static void main(String[] args) {
+               int cpf, senha, opcao;
+               //tela de boas vindas
+               System.out.println("Bem-vindo ao caixa eletrônico");
+               // término de boas vindas
   
-Scanner ret = new Scanner(System.in);
+               Scanner ret = new Scanner(System.in);
 
-        Scanner abc = new Scanner(System.in);
+              Scanner abc = new Scanner(System.in);
 
-            System.out.print("Digite seu cpf : ");
+              System.out.print("Digite seu cpf : ");
                 cpf = abc.nextInt();
                 
       if(cpf == 1234567890){ 
                 
       }else{
           
-          System.out.println("Cpf incorreta! tente novamente!");
+             System.out.println("Cpf incorreta! tente novamente!");
       }
       
-Scanner ler = new Scanner(System.in);
+             Scanner ler = new Scanner(System.in);
 
-        Scanner alf = new Scanner(System.in);
+            Scanner alf = new Scanner(System.in);
 
             System.out.print("Digite sua senha : ");
                 senha = alf.nextInt();
@@ -32,49 +34,50 @@ Scanner ler = new Scanner(System.in);
                 
       }else{
           
-          System.out.println("Senha incorreta! tente novamente!"); // ao falhar a senha retorna nessa mensagem //
+            System.out.println("Senha incorreta! tente novamente!"); // ao falhar a senha retorna nessa mensagem //
       }
- 
-        System.out.println("\tBanco Novo");
-        System.out.println("1 - Saldo");
-        System.out.println("2 - Depósito");
-        System.out.println("3 - Saque");
-        System.out.println("0 - Sair");
+            //menu
+            System.out.println("\tBanco Novo");
+            System.out.println("1 - Saldo");
+            System.out.println("2 - Depósito");
+            System.out.println("3 - Saque");
+            System.out.println("0 - Sair");
 do{
-System.out.print("Escolha sua opção: ");
-opcao = ler.nextInt();
+            System.out.print("Escolha sua opção: ");
+            // término do menu
+       opcao = ler.nextInt();
 
-switch(opcao){
-case 1: //Saldo
-System.out.println("Seu saldo atual é R$1.000,00.");
-break;
+       switch(opcao){
+       case 1: //Saldo
+            System.out.println("Seu saldo atual é R$1.000,00.");
+       break;
 
-case 2: //Depósito
+      case 2: //Depósito
        int deposito; 
         int saldo =  1000;
 
         
-         Scanner bet = new Scanner(System.in);
-            System.out.print("Digite um valor para depositar : ");
+            Scanner bet = new Scanner(System.in);
+       System.out.print("Digite um valor para depositar : ");
              deposito = bet.nextInt();
              
             
             saldo = saldo + deposito;
             
     
-            System.out.println("Seu novo saldo é  R$ " +saldo );
+           System.out.println("Seu novo saldo é  R$ " +saldo );
       
 break;
 
 case 3: // Saque
            int saldo1 =  1000;
-        Scanner tec = new Scanner(System.in);
+          Scanner tec = new Scanner(System.in);
        
-       System.out.println("Digite o valor que deseja sacar: ");
+      System.out.println("Digite o valor que deseja sacar: ");
         int saque = tec.nextInt();
         saque = (saldo1 -saque);
         if(saldo1 <saque){
-            System.out.println("você não possui fundos suficientes! ");
+         System.out.println("você não possui fundos suficientes! ");
             saque = 0;
         }
         
@@ -83,11 +86,11 @@ case 3: // Saque
 break;
 
 case 0: // Sair
-System.out.println("Você saiu com sucesso.");
+       System.out.println("Você saiu com sucesso.");
 break;
 
             default:
-                System.out.println("Opção inválida.");
+      System.out.println("Opção inválida.");
             }
         } while(opcao != 0);
     }
